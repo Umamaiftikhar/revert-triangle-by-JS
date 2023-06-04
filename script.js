724af0057710e4f2667bce69b2538c8772c657db
Umama Iftikhar Siddiqui <124132923+Umamaiftikhar@users.noreply.github.com>
2023-06-04 23:01:54 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+ <script>
+        for(i=1;i<=5;i++)
+        {
+        for(j=5;j>=i;j--)
+        {
+            document.write("*");
+        }
+        document.write("<br>")
+        }
+
+        for(i=1;i<=5;i++)
+        {
+        for(j=5;j>=i;j--)
+        {
+            document.write("1");
+        }
+        document.write("<br>")
+        }
+
+        for(i=1;i<=5;i++)
+        {
+        for(j=5;j>=i;j--)
+        {
+            document.write(j);
+        }
+        document.write("<br>")
+        }
+
+        for(i=1;i<=5;i++)
+        {
+        for(j=5;j>=i;j--)
+        {
+            document.write(i);
+        }
+        document.write("<br>")
+        }
+    </script>
